0a65c629dd50fa3795460e8e97edf6a3818ece34
R Scott Bradshaw <rscottbradshaw@Scotts-MacBook-Air.local>
2015-04-08 11:00:59 -0400

    board sets cells, checks if claimed, and checks if full

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,43 @@
+class Board
 
+attr_reader :cells
+
+  def initialize
+    @cells = [" "," "," "," "," "," "," "," "," "]
+  end
+
+  def set_cell(i, mark)
+    @cells[i] = mark
+  end
+
+  def cell_claimed?(location)
+    if @cells[location] == " "
+      return false
+    else
+      return true
+    end
+  end
+
+  def full?
+    if @cells.include?(" ")
+      return false
+    else
+      return true
+    end
+  end
+
+  def display
+   puts "--------------------"
+   puts "|#{@cells[0]}|#{@cells[1]}|#{@cells[2]}|"
+   puts "|#{@cells[3]}|#{@cells[4]}|#{@cells[5]}|"
+   puts "|#{@cells[6]}|#{@cells[7]}|#{@cells[8]}|"
+   puts "--------------------"
+ end
+
+end
+#
+# b = Board.new
+# b.set_cell(0, "x")
+# b.display
+# b.set_cell(8, "o")
+# b.display
